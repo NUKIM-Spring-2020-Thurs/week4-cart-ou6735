592d41ea69a17279cbcaa426a19be9e8949c1b1f
ou6735 <61553899+ou6735@users.noreply.github.com>
2020-04-09 16:01:18 +0800

    Add files via upload

---
+++ ccheck.php
@@ -0,0 +1,23 @@
+<?php
+ob_start(); 
+session_start();
+
+$uid="a1073302";
+$upwd="3302";
+
+$id=$_POST["id"];
+$pwd=$_POST["pwd"];
+
+
+if($uid==$id && $upwd==$pwd){
+    $_SESSION["login"]="S";
+    $_SESSION["ID"]=$id;
+
+    header('Location:catalog1.php');
+}else{
+    $_SESSION["login"]="F";
+    header('Location:llogin.php');
+}
+
+
+?>
